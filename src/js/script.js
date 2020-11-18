7b5f14c689ce9fb7697c6479585f56863b343d01
Vasily <kellerovjordan@gmail.com>
2020-11-18 13:07:32 +0300

    recommit

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/ar-left.svg"></button>',
+    });
+});
